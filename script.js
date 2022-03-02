692c69ecd4f9d7ed43b7fedd30e765e57e6af3b8
amanc1248 <amanc1248@gmail.com>
2022-03-02 16:22:48 +0545

    Added the function to script.js

--- script.js
+++ script.js
@@ -3,4 +3,37 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
+const generateTarget = () => {
+  return Math.floor(Math.random() * 8 + 1);
+};
 
+// function for compairing guesses
+const compareGuesses = (humanGuess, computerGuess, currentRoundNumber) => {
+  if (
+    humanGuess - currentRoundNumber === computerGuess - currentRoundNumber ||
+    humanGuess - currentRoundNumber < computerGuess - currentRoundNumber
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+// function for updating score
+const updateScore = (winner) => {
+  if (winner === "human") {
+    humanScore++;
+  } else {
+    computerScore++;
+  }
+};
+
+// for next round
+const advanceRound = () => {
+  currentRoundNumber++;
+};
+
+updateScore("human");
+console.log(humanScore); // To confirm that this value increased by 1
+updateScore("computer");
+console.log(computerScore); // To confirm that this value increased by 1
